d34b095c0b89abc0134ed0bfd282016ee218c2d1
maningbo <maningbo@xywy>
2015-07-10 15:45:36 +0800

    mysql

---
+++ testjavacode/WebContent/MySQL/查看系统信息.sql
@@ -0,0 +1,9 @@
+SELECT VERSION();-- 查看版本号
+
+-- show status like '%Connections%';-- 查看连接状态
+
+-- show variables like '%max_connections%';-- 查看最大连接数
+
+-- show processlist;-- 查看连接数详情列表
+
+select SUBSTRING_INDEX(host,':',1) as ip , count(*) from information_schema.processlist group by ip;
