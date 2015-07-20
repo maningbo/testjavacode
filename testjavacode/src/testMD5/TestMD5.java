9b28ea6e0da197a522c9651f90016002ffa1e1c1
maningbo <maningbo@xywy>
2015-07-20 16:08:40 +0800

    md5加密

---
+++ testjavacode/src/testMD5/TestMD5.java
@@ -0,0 +1,16 @@
+package testMD5;
+/**
+ *  TestMD5.java
+ *  Description:
+ *  
+ *  CreateDate: 2015年7月20日 下午2:48:05 
+ * 
+ *  @author 马宁波
+ */
+public class TestMD5 {
+
+	public static void main(String[] args) {
+		System.out.println(MD5Util.encryptByMD5("1", "a").length()+"\n"+MD5Util.encryptByMD5("1", "a"));
+		System.out.println(MD5Util.encryptByMD5("1", "b").length()+"\n"+MD5Util.encryptByMD5("1", "b"));
+	}
+}
