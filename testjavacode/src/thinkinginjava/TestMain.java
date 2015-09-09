69fec2d97db889ccf4abbea95885bf1e4bf120ee
马宁波 <maningbo@xywy.com>
2015-09-09 08:59:06 +0800

    thinkinginjava测试System类相关方法

---
+++ testjavacode/src/thinkinginjava/TestMain.java
@@ -0,0 +1,17 @@
+package thinkinginjava;
+/**
+ *  TestMain.java
+ *  Description:
+ *  
+ *  CreateDate: 2015年9月9日 上午8:29:51 
+ * 
+ *  @author 马宁波
+ */
+public class TestMain{
+	public static void main(String[] args){
+		//System.getProperties().list(System.out);
+		//System.out.println(System.getProperty("user.name"));
+		//System.out.println(System.getProperties());
+		System.out.println(System.getProperty("java.library.path"));
+	}
+}
