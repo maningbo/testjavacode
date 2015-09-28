0cf2f9aadd6ad195caa087d2936f5803f45134e3
马宁波 <maningbo@xywy.com>
2015-09-28 15:54:49 +0800

    添加注释

--- testjavacode/src/designpatterns/iterator/IteratorTest.java
+++ testjavacode/src/designpatterns/iterator/IteratorTest.java
@@ -8,6 +8,9 @@ package designpatterns.iterator;
  *  @author 马宁波
  */
 public class IteratorTest {
+	/* 此处我们貌似模拟了一个集合类的过程，感觉是不是很爽？
+	 * 其实JDK中各个类也都是这些基本的东西，加一些设计模式，再加一些优化放到一起的，
+	 * 只要我们把这些东西学会了，掌握好了，我们也可以写出自己的集合类，甚至框架！*/
 	public static void main(String[] args) {
 		Collection collection = new MyCollection();
 		Iterator it = collection.iterator();
