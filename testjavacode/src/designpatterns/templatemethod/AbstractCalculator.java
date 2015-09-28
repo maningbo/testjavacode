3c214315124c9e3290d176baaa91ce32c07febb6
马宁波 <maningbo@xywy.com>
2015-09-28 14:34:28 +0800

    添加注释

--- testjavacode/src/designpatterns/templatemethod/AbstractCalculator.java
+++ testjavacode/src/designpatterns/templatemethod/AbstractCalculator.java
@@ -8,11 +8,12 @@ package designpatterns.templatemethod;
  *  @author 马宁波
  */
 public abstract class AbstractCalculator {
+	/*主方法，实现对本类其它方法的调用*/
 	public final int calculator(String exp,String opt){
 		int[] arrayint = split(exp, opt);
 		return calculator(arrayint[0], arrayint[1]);
 	}
-	
+	/*被子类重写的方法*/
 	abstract public int calculator(int num1,int num2);
 	
 	public int[] split(String exp,String opt){
