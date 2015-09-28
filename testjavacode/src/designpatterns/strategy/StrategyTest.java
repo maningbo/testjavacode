a399e80e7a3bd08b9ee26609e59004e5a69ac654
马宁波 <maningbo@xywy.com>
2015-09-28 11:54:28 +0800

    添加注释

--- testjavacode/src/designpatterns/strategy/StrategyTest.java
+++ testjavacode/src/designpatterns/strategy/StrategyTest.java
@@ -9,6 +9,8 @@ package designpatterns.strategy;
  */
 public class StrategyTest {
 	public static void main(String[] args) {
+		//策略模式的决定权在用户，系统本身提供不同算法的实现，新增或者删除算法，对各种算法做封装。
+		//因此，策略模式多用在算法决策系统中，外部用户只需要决定用哪个算法即可
 		ICalculator cal = new Plus();
 		System.out.println(cal.calculator("3+5"));
 		ICalculator cal2 = new Minus();
