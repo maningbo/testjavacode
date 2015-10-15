e9ad6335239aceeb3e4b3a8f6815dfff03ace884
马宁波 <maningbo@xywy.com>
2015-10-15 10:03:57 +0800

    一个针对开发者的迷你测验

---
+++ testjavacode/src/经典案例/MyClass.java
@@ -0,0 +1,28 @@
+package 经典案例;
+/**
+ *  MyClass.java
+ *  Description:
+ *  
+ *  CreateDate: 2015年10月14日 下午5:29:31 
+ * 
+ *  @author 马宁波
+ */
+public class MyClass {
+	private String name;
+	public static void main(String[] args) {
+		MyClass m1 = new MyClass();
+		MyClass m2 = new MyClass();
+		
+		m1.name = m2.name = "m1";
+		
+		CallMe(m1,m2);
+		
+		System.out.println(m1.name+"&"+m2.name);
+		System.out.println(m1==m2);
+		System.out.println(m1+"&"+m2);//没有重写toString方法时打印对象的地址
+	}
+	private static void CallMe(MyClass... m){
+		m[0] = m[1];//改编的数据元素（对数据元素的赋值动作），但对象本身没有改变
+		m[1].name = "new name";
+	}
+}
---
+++ testjavacode/src/经典案例/TestTreeMap.java
@@ -0,0 +1,26 @@
+package 经典案例;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *  TestTreeMap.java
+ *  Description:
+ *  
+ *  CreateDate: 2015年10月14日 下午5:48:04 
+ * 
+ *  @author 马宁波
+ */
+public class TestTreeMap {
+	private static final List<String> NAMES = new ArrayList<String>(){{
+		add("John");
+		add("Tony");
+		System.out.println(NAMES);
+	}};
+	public static void main(String[] args) {
+		Map<String,Object> collection = new TreeMap();
+		System.out.println(NAMES);
+	}
+}
