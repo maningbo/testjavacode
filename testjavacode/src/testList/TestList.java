4646b4d1bd467131f87ce81f1a165e724146c349
马宁波 <maningbo@xywy.com>
2015-11-04 16:36:36 +0800

    Test List

--- testjavacode/src/testList/TestList.java
+++ testjavacode/src/testList/TestList.java
@@ -19,6 +19,7 @@ public class TestList {
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<String>();
 		list.add("1");
+		System.out.println(list.size());
 		list.add("2");
 		list.add("3");
 		list.add("4");
