87f1bfdf8ab1521ce802611ebe9185f48674d179
马宁波 <maningbo@xywy.com>
2015-11-05 09:29:38 +0800

    查询当前jvm内存大小

---
+++ testjavacode/src/environment/TestMemory.java
@@ -0,0 +1,22 @@
+package environment;
+
+/**
+ * TestMemory.java Description:
+ * 
+ * CreateDate: 2015年11月5日 上午9:22:33
+ * 
+ * @author 马宁波
+ */
+public class TestMemory {
+
+	public static String toMemoryInfo() {
+		Runtime currRuntime = Runtime.getRuntime();
+		int nFreeMemory = (int) (currRuntime.freeMemory() / 1024 / 1024);
+		int nTotalMemory = (int) (currRuntime.totalMemory() / 1024 / 1024);
+		return nFreeMemory + "M/" + nTotalMemory + "M(free/total)";
+	}
+
+	public static void main(String[] args) {
+		System.out.println("内存信息：" + toMemoryInfo());
+	}
+}
