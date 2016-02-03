aa22238c6069a0dd19b9a1b305712e4732f6a0ee
马宁波 <maningbo@xywy.com>
2016-02-03 14:52:40 +0800

    printf,Queue

---
+++ testjavacode/src/testjavaclass/system/Printf.java
@@ -0,0 +1,14 @@
+package testjavaclass.system;
+/**
+ *  Printf.java
+ *  Description:
+ *  
+ *  CreateDate: 2016年2月3日 下午2:50:28 
+ * 
+ *  @author 马宁波
+ */
+public class Printf {
+	public static void main(String[] args) {
+		System.out.printf("erer%s，%s\n", 9,"你");
+	}
+}
