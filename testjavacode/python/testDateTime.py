2229b0c9fea40378fab54283e254f5cc46b32cf2
马宁波 <maningbo@xywy.com>
2016-03-24 18:33:13 +0800

    增加python测试类，添加忽略文件

---
+++ testjavacode/python/testDateTime.py
@@ -0,0 +1,35 @@
+#coding:utf-8
+'''
+Created on 2016年3月24日
+
+@author: maningbo
+'''
+
+from datetime import timedelta,date
+import time
+from calendar import monthrange
+
+dt='2016-03-24'
+timestamp = int(time.mktime(time.strptime(str(dt), "%Y-%m-%d")))
+print dt
+print timestamp
+timesplit=dt.split("-")
+day=date(int(timesplit[0]),int(timesplit[1]),int(timesplit[2]))
+goal = timedelta(days=-60)
+goalday = day + goal
+data=goalday.strftime('%Y-%m-%d')
+print data
+
+monthlater = day + timedelta(days=monthrange(day.year,day.month)[1])
+print monthlater.strftime('%Y-%m-%d')
+print '-----------------------------------------------------'
+dt2='2016-04-01'
+timestamp2 = int(time.mktime(time.strptime(str(dt), "%Y-%m-%d")))
+print dt2
+print timestamp2
+timesplit2=dt2.split("-")
+day2 = date(int(timesplit2[0]),int(timesplit2[1]),int(timesplit2[2]))
+goal2 = timedelta(days=-1)
+goalday2 = day2 + goal2
+data2=goalday2.strftime('%Y-%m')
+print data2+'-01'
