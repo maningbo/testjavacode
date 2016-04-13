b2c3f64249d875b9c0bd71f186d4c1921258dc2a
马宁波 <maningbo@xywy.com>
2016-04-13 08:51:58 +0800

    +

---
+++ testjavacode/src/testjavaclass/TestTimeAndDate.java
@@ -0,0 +1,15 @@
+package testjavaclass;
+
+/**
+ * TestTimeAndDate.java<br>
+ * Description:<br>
+ * <br>
+ * CreateDate: 2016年4月13日 上午8:46:53
+ * 
+ * @author 马宁波
+ */
+public class TestTimeAndDate {
+	public static void main(String[] args) {
+		System.out.println(System.currentTimeMillis());
+	}
+}
