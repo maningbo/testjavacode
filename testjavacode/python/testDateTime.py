fde336a1897dabcb4da779c1fb50da604e6a4f14
马宁波 <maningbo@xywy.com>
2016-04-14 14:47:27 +0800

    引入sys，向脚本传递参数

--- testjavacode/python/testDateTime.py
+++ testjavacode/python/testDateTime.py
@@ -9,31 +9,33 @@ from datetime import timedelta,date
 import time
 from calendar import monthrange
 import os
+import sys
 
-print sys.argv[1]
-os.system("echo 343242342")
-
-dt='2016-03-24'
-timestamp = int(time.mktime(time.strptime(str(dt), "%Y-%m-%d")))
-print dt
-print timestamp
-timesplit=dt.split("-")
-day=date(int(timesplit[0]),int(timesplit[1]),int(timesplit[2]))
-goal = timedelta(days=-60)
-goalday = day + goal
-data=goalday.strftime('%Y-%m-%d')
-print data
-
-monthlater = day + timedelta(days=monthrange(day.year,day.month)[1])
-print monthlater.strftime('%Y-%m-%d')
-print '-----------------------------------------------------'
-dt2='2016-04-01'
-timestamp2 = int(time.mktime(time.strptime(str(dt), "%Y-%m-%d")))
-print dt2
-print timestamp2
-timesplit2=dt2.split("-")
-day2 = date(int(timesplit2[0]),int(timesplit2[1]),int(timesplit2[2]))
-goal2 = timedelta(days=-1)
-goalday2 = day2 + goal2
-data2=goalday2.strftime('%Y-%m')
-print data2+'-01'
+if __name__=='__main__':
+    print sys.argv[1]
+    os.system("echo 343242342")
+    
+    dt='2016-03-24'
+    timestamp = int(time.mktime(time.strptime(str(dt), "%Y-%m-%d")))
+    print dt
+    print timestamp
+    timesplit=dt.split("-")
+    day=date(int(timesplit[0]),int(timesplit[1]),int(timesplit[2]))
+    goal = timedelta(days=-60)
+    goalday = day + goal
+    data=goalday.strftime('%Y-%m-%d')
+    print data
+    
+    monthlater = day + timedelta(days=monthrange(day.year,day.month)[1])
+    print monthlater.strftime('%Y-%m-%d')
+    print '-----------------------------------------------------'
+    dt2='2016-04-01'
+    timestamp2 = int(time.mktime(time.strptime(str(dt), "%Y-%m-%d")))
+    print dt2
+    print timestamp2
+    timesplit2=dt2.split("-")
+    day2 = date(int(timesplit2[0]),int(timesplit2[1]),int(timesplit2[2]))
+    goal2 = timedelta(days=-1)
+    goalday2 = day2 + goal2
+    data2=goalday2.strftime('%Y-%m')
+    print data2+'-01'
