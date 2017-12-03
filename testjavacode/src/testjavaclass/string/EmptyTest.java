c20350e67a6c505c75e61e70dea4ae4329312e68
马宁波 <maningbo@xywy.com>
2017-12-03 17:58:22 +0800

    +

---
+++ testjavacode/src/testjavaclass/string/EmptyTest.java
@@ -0,0 +1,15 @@
+package testjavaclass.string;
+/**
+ *  EmptyTest.java<br>
+ *  Description:<br>
+ *  <br>
+ *  CreateDate: 2017年12月3日 下午3:56:58
+ *  @author 马宁波
+ */
+public class EmptyTest {
+    public static void main(String[] args) {
+        String s = null;
+        s = "";
+        System.out.println(s.isEmpty());
+    }
+}
